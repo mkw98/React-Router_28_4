66ddcdb1ebae8cf77a7730313036406436672a6b
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-06 15:08:23 +0100

    add components edit scripts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import Navigation from './presentational/navigation.component';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+//import { getCountries } from './actions/actions-countries';
+
 
 render(
 	<Provider store={store}>
-		<div>
-			<h1>Inicjalizacja projektu</h1>
-			<DevTools />
-		</div>
+		<Router history={hashHistory}>
+			<Route path='/' component={Navigation}>
+
+			</Route>
+		</Router>
 	</Provider>,
 	document.getElementById('root')
 );
 
-store.dispatch(getCountries());
+//(
+//	<Provider store={store}>
+//		<div>
+//			<h1>Inicjalizacja projektu</h1>
+//			<DevTools />
+//		</div>
+//	</Provider>,
+//	document.getElementById('root')
+//);
+
+//store.dispatch(getCountries());
+
+
+
