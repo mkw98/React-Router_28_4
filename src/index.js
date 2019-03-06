f31c7ac1f7aed6d1cb31ea8158d84fa63899993d
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-06 17:37:52 +0100

    instll older bootstrap version to fix css problem

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import Navigation from './presentational/navigation.component';
+import routes from './routes';
 import DevTools from './DevTools';
 //import { getCountries } from './actions/actions-countries';
 
-
 render(
-	<Provider store={store}>
-		<Router history={hashHistory}>
-			<Route path='/' component={Navigation}>
-
-			</Route>
-		</Router>
-	</Provider>,
-	document.getElementById('root')
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
 );
 
-//(
-//	<Provider store={store}>
-//		<div>
-//			<h1>Inicjalizacja projektu</h1>
-//			<DevTools />
-//		</div>
-//	</Provider>,
-//	document.getElementById('root')
-//);
 
 //store.dispatch(getCountries());
 
